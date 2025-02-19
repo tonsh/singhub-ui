aac7d777432b6a449c258059f32975a5f4ed38c9
tonshlee <tonshlee@gmail.com>
2025-02-19 13:44:21 +0000

    install shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import trailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -8,4 +9,9 @@ export default defineConfig({
     react(),
     trailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
