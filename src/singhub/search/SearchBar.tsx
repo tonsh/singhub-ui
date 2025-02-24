f5dff6de2ba73f2d734ab6450cb20e0db1336ee2
tonshlee <tonshlee@gmail.com>
2025-02-24 12:57:41 +0000

    Install Ant Design UI

---
+++ src/singhub/search/SearchBar.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+export default function SearchBar() {
+  return (
+    <>
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    </>
+  );
+}
