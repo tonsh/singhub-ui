3a30daf84a852119388761e425627f00380aa992
tonshlee <tonshlee@gmail.com>
2025-03-11 18:24:24 +0000

    GroupSelector Component

---
+++ src/singhub/search/GroupSelector.tsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from 'react';
+import { Select, Space, ConfigProvider } from 'antd';
+
+interface GroupItem {
+  uuid: string;
+  name: string;
+}
+
+interface GroupData {
+  group: Array<GroupItem>;
+  items: Array<GroupItem>;
+}
+
+interface SelectorOption {
+  value: string;
+  label: string;
+}
+
+interface SelectorData {
+  [key: string]: SelectorOption[];
+}
+
+const ArrowDownIcon = () => {
+  return (
+    <svg fill="none" viewBox="0 0 24 24">
+    <path d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+    </svg>
+  );
+};
+
+
+export default function GroupSelector({ uri, label, groupPlaceholder, placeholder }: { uri: string, label: string, groupPlaceholder: string, placeholder: string }) {
+  const [data, setData] = useState<SelectorData>({});
+  const [groups, setGroups] = useState<SelectorOption[]>([]);
+  const [items, setItems] = useState<SelectorOption[]>([]);
+  const [selectedItem, setSelectedItem] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(`${import.meta.env.VITE_SINGHUB_API_URL}${uri}`);
+        const result: GroupData[] = await response.json();
+
+        const data: SelectorData = {};
+        const groups: SelectorOption[] = [];
+        result.forEach((item: GroupData) => {
+            data[item.group.uuid] = item.items.map((opt: GroupItem) => ({value: opt.uuid, label: opt.name}));
+            groups.push({value: item.group.uuid, label: item.group.name});
+        });
+        setData(data);
+        setGroups(groups);
+      } catch (error) {
+        console.error('获取数据失败:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [uri]);
+
+  const onGroupChange = (value: string) => {
+    if (data[value]) {
+      setItems(data[value]);
+      setSelectedItem(undefined);
+    }else {
+      // 当清除组选择时，也清空 Items 选项
+      setItems([]);
+      setSelectedItem(undefined);
+    }
+  };
+
+  return (
+    <>
+      <div className="filter-row">
+        <span className="filter-label">{label}: </span>
+        <ConfigProvider theme={{
+            components: {
+            Select: {
+                activeBorderColor: 'var(--color-gray-400)',
+                hoverBorderColor: 'var(--border-color)',
+                activeOutlineColor: 'none',  // 移除阴影
+            }
+            }}}>
+            <Space direction="horizontal" size={12}>
+                <Select
+                    options={groups}
+                    loading={loading}
+                    placeholder={groupPlaceholder}
+                    allowClear={true}
+                    suffixIcon={<ArrowDownIcon />}
+                    onChange={onGroupChange}
+                    style={{width: '170px'}}
+                />
+                <Select
+                    options={items}
+                    loading={loading}
+                    placeholder={placeholder}
+                    allowClear={true}
+                    suffixIcon={<ArrowDownIcon />}
+                    value={selectedItem}
+                    onChange={(value) => setSelectedItem(value)}
+                    style={{width: '350px'}}
+                />
+            </Space>
+        </ConfigProvider>
+      </div>
+    </>
+  );
+}
--- src/singhub/search/SearchBar.tsx
+++ src/singhub/search/SearchBar.tsx
@@ -1,22 +1,6 @@
 import { useState } from 'react';
-import { Space, DatePicker, ConfigProvider } from 'antd';
-import dayjs from 'dayjs';
-
-const DateIcon = () => {
-  return (
-    <svg fill="none" viewBox="0 0 24 24">
-      <path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-    </svg>
-  );
-};
-
-const SeparatorIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-    </svg>
-  );
-};
+import GroupSelector from './GroupSelector';
+import DateRange from './DateRange';
 
 export default function SearchBar() {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
@@ -56,28 +40,9 @@ export default function SearchBar() {
         </div>
 
         <div className={`filter-panel${!isFilterOpen ? ' hide' : ''}`}>
-          <ConfigProvider theme={{
-            components: {
-              DatePicker: {
-                activeBorderColor: 'var(--color-gray-400)',
-                hoverBorderColor: 'var(--border-color)',
-                activeShadow: 'none',  // 移除阴影
-              }
-            }
-          }}>
-            <Space direction="vertical" size={12}>
-              <DatePicker.RangePicker
-                className="filter-date-picker"
-                placeholder={['Start Date', 'End Date']}
-                minDate={dayjs(new Date())}
-                maxDate={dayjs(new Date()).add(6, 'months')}
-                format='MMM DD, YYYY'
-                popupStyle={{ '--active-bar-bg': 'var(--color-red-600)' } as React.CSSProperties}
-                suffixIcon={<DateIcon />}
-                separator={<SeparatorIcon />}
-              />
-            </Space>
-          </ConfigProvider>
+          <GroupSelector uri="/school/cca/" label="CCA" groupPlaceholder="Category" placeholder="Co-curricular activities" />
+          <GroupSelector uri="/location/" label="Loc" groupPlaceholder="Region" placeholder="Venue" />
+          <DateRange />
         </div>
       </div>
     </>
